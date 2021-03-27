0d7af484c28408f8f28148e6c3b630f7eff2f6ed
Razei <razeixyz@gmail.com>
2021-03-27 18:54:50 -0400

    Added JPA service project

---
+++ SpringBootJPAServiceAssign4App/src/main/java/com/spring/jpa/SpringBootJpaServiceAssign4AppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaServiceAssign4AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaServiceAssign4AppApplication.class, args);
+	}
+
+}
