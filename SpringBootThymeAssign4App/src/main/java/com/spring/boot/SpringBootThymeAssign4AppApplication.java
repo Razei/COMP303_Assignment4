24c3987102b5ece306d1383a9d70bd7212f4f04e
Razei <razeixyz@gmail.com>
2021-03-27 18:05:59 -0400

    Added Thymeleaf project

---
+++ SpringBootThymeAssign4App/src/main/java/com/spring/boot/SpringBootThymeAssign4AppApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeAssign4AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeAssign4AppApplication.class, args);
+		System.out.println("Spring Boot Thymeleaf app is ready at http://localhost:8080");
+	}
+
+}
