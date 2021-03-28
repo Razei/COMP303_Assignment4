bb65e875421ef214be41cd8b3410dad5cd606e8f
Alvin <asjyap29@hotmail.com>
2021-03-28 07:53:40 -0400

    Added patient microservices and connected it

---
+++ SpringBootJPAServiceAssign4App/src/main/java/com/spring/jpa/Patient.java
@@ -0,0 +1,93 @@
+package com.spring.jpa;
+
+public class Patient {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String gender;
+	private int ohip_ID;
+	private String address;
+	private String city;
+	private String phone;
+	
+	public Patient() {
+		super();
+	}
+	
+	public Patient(String firstName, String lastName, int age, String gender,int ohip,String address, String city, String phone) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gender = gender;
+		this.ohip_ID = ohip;
+		this.address = address;
+		this.city = city;
+		this.phone = phone;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getOhip_ID() {
+		return ohip_ID;
+	}
+
+	public void setOhip_ID(int ohip_ID) {
+		this.ohip_ID = ohip_ID;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
--- SpringBootJPAServiceAssign4App/src/main/java/com/spring/jpa/SpringBootJpaServiceAssign4AppApplication.java
+++ SpringBootJPAServiceAssign4App/src/main/java/com/spring/jpa/SpringBootJpaServiceAssign4AppApplication.java
@@ -3,11 +3,20 @@ package com.spring.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringBootJpaServiceAssign4AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaServiceAssign4AppApplication.class, args);
+		System.out.println("Eureka Server started to register a micro-service");
 	}
 
 }
