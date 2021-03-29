515eafbbecf6b3709f2634968cbf440526cdde14
Alvin <asjyap29@hotmail.com>
2021-03-29 08:23:38 -0400

    Updated Microservice

--- SpringBootJPAServiceAssign4App/src/main/java/com/spring/jpa/SpringBootJpaServiceAssign4AppApplication.java
+++ SpringBootJPAServiceAssign4App/src/main/java/com/spring/jpa/SpringBootJpaServiceAssign4AppApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
+
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringBootJpaServiceAssign4AppApplication {
