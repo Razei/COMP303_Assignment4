fd4966ebe0ead27c2ae35e42e19d03a2e557a7e0
Razei <razeixyz@gmail.com>
2021-03-29 17:29:18 -0400

    Created homepage

--- SpringBootThymeAssign4App/src/main/java/com/spring/boot/SpringBootThymeAssign4AppApplication.java
+++ SpringBootThymeAssign4App/src/main/java/com/spring/boot/SpringBootThymeAssign4AppApplication.java
@@ -2,13 +2,19 @@ package com.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootThymeAssign4AppApplication {
+public class SpringBootThymeAssign4AppApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeAssign4AppApplication.class, args);
 		System.out.println("Spring Boot Thymeleaf app is ready at http://localhost:8080");
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 }
