9ea3910defee228bb91d551af1e55eee59c48cb6
Alvin <asjyap29@hotmail.com>
2021-03-30 00:49:11 -0400

    Updated Patient Service and CRUD

--- SpringBootJPAPatientServiceAssign4App/src/main/java/com/spring/jpa/PatientController.java
+++ SpringBootJPAPatientServiceAssign4App/src/main/java/com/spring/jpa/PatientController.java
@@ -1,6 +1,12 @@
 package com.spring.jpa;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,31 +18,32 @@ public class PatientController {
 	public PatientController(PatientService patientService) {
 		this.patientService = patientService;
 	}
-
-	@RequestMapping(value ="/patient", method = RequestMethod.GET)
-	public Patient create() {
-		try {
-			Patient patient = new Patient();
-
-			patient.setFirstName("John");
-			patient.setLastName("Doe");
-			patient.setAddress("Somewhere Drive");
-			patient.setAge(25);
-			patient.setCity("Happyville");
-			patient.setGender("M");
-			patient.setOhip_ID(1312513);
-			patient.setPhone("905-554-0686");
-
-			patientService.addPatient(patient);
-			return patient;
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
-		}
+	
+	//add a patient object to the repo
+	@RequestMapping(value ="/patient", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 @ResponseStatus(value = HttpStatus.OK)
+	public void addPatient(@RequestBody Patient p) throws Exception {
+		 patientService.addPatient(p);
+		
+		
 	}
+	
+	
+	 @RequestMapping(value = "/patient/{OHIP_ID}", method = RequestMethod.GET)
+	 Patient getPatient(@PathVariable("OHIP_ID") int OHIP_ID) throws Exception {
+	     return patientService.getPatient(OHIP_ID);
+	 }
 
 	@RequestMapping(value ="/patients", method = RequestMethod.GET)
 	public List<Patient> getPatients(){
 		return patientService.getPatients();
 	}
+	
+	
+	@RequestMapping(value = "/patient/{OHIP_ID}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 @ResponseStatus(value = HttpStatus.OK)
+	 void daletePatient(@PathVariable("OHIP_ID") int OHIP_ID, @RequestBody Patient pat) throws Exception {
+	    
+	     patientService.deletePatient(OHIP_ID);
+	 }
 }
